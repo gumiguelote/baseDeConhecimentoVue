9e902512fa3e03517e2be17e7dcb313c6792ef78
Gustavo Miguelote <gustavomiguelote@me.com>
2019-06-02 21:58:59 -0300

    incluiding mongodb on project and schedule events

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,11 @@
 const app =  require('express')();
 const consign = require('consign');
 const db = require('./config/db');
+const mongoose = require('mongoose');
+require('./config/mongodb');
 
 app.db = db;
+app.mongoose = mongoose;
 
 consign()
     .include('./config/passport.js')
@@ -10,6 +13,7 @@ consign()
     .then('./api/validation.js')
     .then('./api')
     .then('./config/routes.js')
+    //.then('./schedule/statsSchedule.js')
     .into(app)
 
 app.listen(3000, () => {
