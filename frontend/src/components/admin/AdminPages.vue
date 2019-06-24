1613a8115b877cd8a8908dda204647335c41a3ac
Gustavo Miguelote <gustavomiguelote@me.com>
2019-06-23 23:06:14 -0300

    desenvolvimento do frontend, Pequenas mudanças no backend

---
+++ frontend/src/components/admin/AdminPages.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="admin-pages">
+    <PageTitle icon="fa fa-cogs" main="Administração de Sistema" sub="Cadastros & Cia"/>
+    <div class="admin-pages-tabs">
+      <b-card no-body>
+          <b-tabs card>
+              <b-tab title="Artigos" active="">
+                  <ArticleAdmin />
+              </b-tab>
+              <b-tab title="Categorias" active="">
+                  <CategoryAdmin />
+              </b-tab>
+              <b-tab title="Usuários" active="">
+                  <UserAdmin />
+              </b-tab>                            
+          </b-tabs>
+      </b-card>
+    </div>
+  </div>
+</template>
+
+<script>
+import PageTitle from "@/components/template/PageTitle";
+import ArticleAdmin from "@/components/admin/ArticleAdmin"
+import CategoryAdmin from "@/components/admin/CategoryAdmin"
+import UserAdmin from "@/components/admin/UserAdmin"
+export default {
+  name: "AdminPages",
+  components: { PageTitle, ArticleAdmin, CategoryAdmin, UserAdmin }
+};
+</script>
+
+<style>
+</style>
--- frontend/src/config/store.js
+++ frontend/src/config/store.js
@@ -1,24 +1,36 @@
+import axios from 'axios'
 import Vue from 'vue'
 import Vuex from 'vuex'
 
-Vue.use(Vuex);
+Vue.use(Vuex)
 
 export default new Vuex.Store({
     state: {
-        isMenuVisible: true,
-        user: {
-            name:'Usuário Hardcoded',
-            email:'gustavomiguelote@me.com'
-        }
+        isMenuVisible: false,
+        user: null
     },
     mutations: {
-        toggleMenu(state, isMenuVisible){
-            if (isMenuVisible === undefined) {
-                state.isMenuVisible = !state.isMenuVisible;
-            } else{ 
-                state.isMenuVisible = isMenuVisible;
+        toggleMenu(state, isVisible) {
+            if(!state.user) {
+                state.isMenuVisible = false
+                return
+            }
+
+            if(isVisible === undefined) {
+                state.isMenuVisible = !state.isMenuVisible
+            } else {
+                state.isMenuVisible = isVisible
+            }
+        },
+        setUser(state, user) {
+            state.user = user
+            if(user) {
+                axios.defaults.headers.common['Authorization'] = `bearer ${user.token}`
+                state.isMenuVisible = true
+            } else {
+                delete axios.defaults.headers.common['Authorization']
+                state.isMenuVisible = false
             }
         }
     }
 })
-
